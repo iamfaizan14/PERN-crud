dacc8a575e233fa185e8a84afe466da8530b234f
iamfaizan14 <abdulfaizan3939@gmail.com>
2023-06-29 18:34:57 +0530

    initial commit for backend code

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 const pool = new Pool({
   user: "postgres",
-  password: "Tekisky@123",
+  password: "Password@123",
   host: "localhost",
   port: 5432,
   database: "perntodo",
